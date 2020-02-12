3784ae74d7855b5e88f67da0941cc42521453b01
reganfbernas <regan.bernas@gmail.com>
2020-02-12 20:04:21 +0800

    data

---
+++ Action.php
@@ -0,0 +1,10 @@
+<?php
+ $id = $_post['id'];
+ $Emotion = $_post['Emotion'];
+ $Quote = $_post['Quote'];
+ 
+ if (!empty($id)) || !empty($Emotion) || !empty($Quote)
+ {
+	 $host= "localhost";
+	 $
+ }
---
+++ Emotions.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2020 at 12:57 PM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `emodb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Emotions`
+--
+
+CREATE TABLE `emotions` (
+  `id` int(11) NOT NULL,
+  `Emotion` varchar(200) NOT NULL,
+  `Quote` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
